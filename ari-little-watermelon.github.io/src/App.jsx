1153a331ec3924062c6ed0c2b04e67f2374df559
Arundhati Bandopadhyaya <132263094+arundhati-work@users.noreply.github.com>
2025-02-15 21:55:53 -0500

    added navbar back

--- ari-little-watermelon.github.io/src/App.jsx
+++ ari-little-watermelon.github.io/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import ScrollComponent from './Components/ScrollComponent/ScrollComponent';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
+    <div id="screen">
+    <Navbar/>
     <ScrollComponent/>
+    </div>
   )
 }
 
